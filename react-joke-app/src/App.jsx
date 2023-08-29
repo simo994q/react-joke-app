852323013167e052ee8bf8152c274cfec5712745
simo994q <simonkn1@hotmail.com>
2023-08-29 09:17:52 +0200

    app

---
+++ react-joke-app/src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [joke, setJoke] = useState({})
+  const [newJokeState, setNewJokeState] = useState(false)
+
+  useEffect(() => {
+    const url = 'https://official-joke-api.appspot.com/random_joke'
+    fetch(url).then(res => res.json()).then(data => setJoke(data))
+  }, [newJokeState])
+
+
+  return (
+    <>
+      <h2>{joke.setup}</h2>
+      <h3>{joke.punchline}</h3>
+      <button onClick={() => {newJokeState ? setNewJokeState(false) : setNewJokeState(true)}}>New joke</button>
+    </>
+  )
+}
+
+export default App
